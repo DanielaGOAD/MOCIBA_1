979a93f6cbd67db45b3639a5efbe9f567121342b
DanielaGOAD <39204468@uaz.edu.mx>
2026-07-03 23:20:50 -0600

    Update APP_M.py

--- APP_M.py
+++ APP_M.py
@@ -93,7 +93,7 @@ def cargar_datos_base():
     file_ids = [
         "1ojZcLZost0BM00yCGN8OLnu7XYyLpEYr",
         "1v08-7Jx4Iw3msAkgb1m01Im49s8gqCqO",
-        "1UjJeym8BHKD1OnUh-T6An6JWt9Ikl0mP"
+        "1v6OgDHRSqNHbDnph2ijFvZB6RyTBAAk8"
     ]
 
     columnas_p5 = [f"P5_{i:02d}_{j}" for i in range(1, 14) for j in range(1, 4)]
